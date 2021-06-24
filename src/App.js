1d1f9afe2411204842a2b1399024c0e79220f502
steffiehompes <82502716+steffiehompes@users.noreply.github.com>
2021-06-24 18:39:17 +0200

    Removed unnecessary files and updated bootstrap import

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ import "./App.css";
 import Search from "./Search";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <div className="WeatherApplication">
-        <Search />
-        <div className="row Forecast">
-          <div className="col-2">
-            <Forecast />
-          </div>
-          <div className="col-2">
-            <Forecast />
-          </div>
-          <div className="col-2">
-            <Forecast />
-          </div>
-          <div className="col-2">
-            <Forecast />
-          </div>
-          <div className="col-2">
-            <Forecast />
-          </div>
-          <div className="col-2">
-            <Forecast />
+      <div className="container">
+        <div className="WeatherApplication">
+          <Search />
+          <div className="row Forecast">
+            <div className="col-2">
+              <Forecast />
+            </div>
+            <div className="col-2">
+              <Forecast />
+            </div>
+            <div className="col-2">
+              <Forecast />
+            </div>
+            <div className="col-2">
+              <Forecast />
+            </div>
+            <div className="col-2">
+              <Forecast />
+            </div>
+            <div className="col-2">
+              <Forecast />
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
-
-export default App;
